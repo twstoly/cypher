548e0df8b09576e560f6aeed8ca9ff8ef351421a
Oliver <oliver.mwangale@moringaschool.com>
2022-01-22 17:40:52 +0300

    decryption Functionality

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -11,7 +11,55 @@ public class Decrypt {
     }
 
     public String decryptionMaster(String textInput, int key){
+        for (int i = 0; i < textInput.length(); i++){
 
+//             Checking  that the key value is within the range before starting encrypting;
+            if (key >= 1 && key <= 25){
+
+//                Getting the characters value from the position
+                char characterValue = textInput.charAt(i);
+
+//                Getting the right value for the encryption key if lowercase then lowercase is used.
+                int lowercaseCharacterIndex = lowercaseAlphabets.indexOf(textInput.charAt(i));
+                int uppercaseCharacterIndex = uppercaseAlphabets.indexOf(textInput.charAt(i));
+
+//                Check if the character is a letter or any other value
+                if (Character.isLetter(characterValue)){
+
+//                    checking if the character value is a lowercase letter or uppercase letter
+                    if (Character.isLowerCase(characterValue)){
+//                      Using the index position of the character
+                        int newCharacterIndex = (lowercaseCharacterIndex - key) % 26;
+                        if(newCharacterIndex < 0){
+                            newCharacterIndex = lowercaseAlphabets.length() + newCharacterIndex;
+                        }
+
+//                        Equating the value of the index and matching it with the character in the lowercaseAlphabet
+                        char characterPositionOfNewIndex = lowercaseAlphabets.charAt(newCharacterIndex);
+
+                        cipherText += characterPositionOfNewIndex;
+
+                    }else if (Character.isUpperCase(characterValue)){
+
+                        int newCharacterIndex = (uppercaseCharacterIndex - key) % 26;
+                        if (newCharacterIndex < 0){
+                            newCharacterIndex = uppercaseAlphabets.length() + newCharacterIndex;
+                        }
+                        char characterPositionOfNewIndex = uppercaseAlphabets.charAt(newCharacterIndex);
+
+//                        Assigning the encrypted of the alphabet to the cipher text
+                        cipherText += characterPositionOfNewIndex;
+                    }
+                }else {
+
+//                    If the inputValue is not a letter we jst want to add it in the application
+                    cipherText += characterValue;
+                }
+
+            }else{
+                System.out.println("Key values range from 1 to 25");
+            }
+        }
         return cipherText;
     }
 
