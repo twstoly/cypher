c356f08ff5f6c8b6f6453a7ced7c3c09d4d49e9a
Oliver <oliver.mwangale@moringaschool.com>
2022-01-22 16:46:10 +0300

    decryption

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,130 @@
 public class Decrypt {
+    private String textInput;
+    private int key;
+    private String outputLowercase;
+    private String cipherText = "";
+    private String uppercaseAlphabets = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+    private String lowercaseAlphabets = "zyxwvutsrqponmlkjihgfedcba";
+
+    public Decrypt() {
+
+    }
+
+    public String decryptionMaster(String textInput, int key){
+
+        return cipherText;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
