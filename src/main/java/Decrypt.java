706eaaad4dcd3bbe53ce4db105bf4a8e58c1c810
Oliver <oliver.mwangale@moringaschool.com>
2022-01-22 16:47:05 +0300

    Adding getters and setters in decryption class

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -15,6 +15,53 @@ public class Decrypt {
         return cipherText;
     }
 
+    public String getTextInput() {
+        return textInput;
+    }
+
+    public void setTextInput(String textInput) {
+        this.textInput = textInput;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getOutputLowercase() {
+        return outputLowercase;
+    }
+
+    public void setOutputLowercase(String outputLowercase) {
+        this.outputLowercase = outputLowercase;
+    }
+
+    public String getCipherText() {
+        return cipherText;
+    }
+
+    public void setCipherText(String cipherText) {
+        this.cipherText = cipherText;
+    }
+
+    public String getUppercaseAlphabets() {
+        return uppercaseAlphabets;
+    }
+
+    public void setUppercaseAlphabets(String uppercaseAlphabets) {
+        this.uppercaseAlphabets = uppercaseAlphabets;
+    }
+
+    public String getLowercaseAlphabets() {
+        return lowercaseAlphabets;
+    }
+
+    public void setLowercaseAlphabets(String lowercaseAlphabets) {
+        this.lowercaseAlphabets = lowercaseAlphabets;
+    }
 }
 
 
